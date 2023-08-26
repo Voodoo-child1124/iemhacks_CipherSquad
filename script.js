afa6906b52319f5952d1564cd2306566b8c1648f
Ayan <142825708+AD9190@users.noreply.github.com>
2023-08-26 23:31:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let text1 = document.getElementById("text");
 let text = document.getElementById("text");
 window.addEventListener('scroll',() =>
 {
     let value = window.scrollY;
-    text1.style.marginTop= value*-2.5+'px';
     text.style.marginTop= value*2.5+'px';
     }
 );
