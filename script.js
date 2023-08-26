4fa586c23dbba95fa54615de6386d93cfb2a404f
Ayan <142825708+AD9190@users.noreply.github.com>
2023-08-27 03:59:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let text = document.getElementById("text");
+let fg= document.getElementById("fg");
+let water = document.getElementById("water");
+let sky = document.getElementById("sky");
 window.addEventListener('scroll',() =>
 {
     let value = window.scrollY;
+
+
     text.style.marginTop= value*2.5+'px';
-    }
+    fg.style.top = value*-1.5+'px';
+    fg.style.left = value*-1.5+'px';
+    sky.style.left = value*1.5+'px';
+    water.style.right = value*-1.5+'px';
+
+
+}
 );
